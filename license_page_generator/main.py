3a0970d32d6b6fca88a4670738f4ac5ec84afcaa
Fang-Pen Lin <hello@fangpenlin.com>
2021-12-04 17:26:47 -0800

    When other license is returned, use the value from nlf instead

--- license_page_generator/main.py
+++ license_page_generator/main.py
@@ -157,10 +157,14 @@ def main(
 
             if retry_count == 3:
                 raise RuntimeError("Failed to get github repo, please try again later")
+            if not first_input_license:
+                first_input_license = "Other"
             writer.writerow(
                 dict(
                     name=name,
-                    license=license.name or first_input_license,
+                    license=license.name
+                    if license.name and license.name != "Other"
+                    else first_input_license,
                     license_url=license.url_to_license,
                 )
             )
